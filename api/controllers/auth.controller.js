537754b2e1613dd7e02493c6e6bdaabc948bac89
Ranjit Madame <ranjitkumar.madame@mphasis.com>
2024-02-07 17:45:40 +0530

    Create sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     // we want to get the information from browser(client=req) which is coming from body
@@ -19,4 +20,26 @@ export const signup = async (req, res, next) => {
         next(error); // => api/index.js => app.use((err, req, res, next)
         // next(errorHandler(500, 'Error from the function that we created!')) // manual error
     }    
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        // Check if user is valid
+        const validUser = await User.findOne({ email });
+        if( !validUser) return next(errorHandler(404, 'Wrong credentials!'));
+        // Check if password is correct
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if( !validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        // Create token
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        // Remove the password before sending it back even it is encrupted. Sending password is not a good practice
+        const {password: pw, ...rest} = validUser._doc; // seperate the password. We still get password if dont use _doc
+        // Save this token as cookie and retun rest which is without password and not the validUser
+        // res.cookie('access_token', token, {httpOnly: true}).status(200).json(validUser);
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+
+    } catch (error) {
+        next(error); 
+    }
 }
