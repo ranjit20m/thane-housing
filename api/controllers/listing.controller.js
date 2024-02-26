de2b993f05d8e4f9f19beb2f5edd6f9a8cfd4bd7
Ranjit Madame <ranjitkumar.madame@mphasis.com>
2024-02-26 20:11:33 +0530

    Complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,10 +1,28 @@
-import Listing from "../models/listing.model.js";
+import Listing from "../models/listing.model.js"; 
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing); // 201 => something is created
         
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteListing = async (req, res, next) => {
+    // check if the listing is present or not
+    const listing = await Listing.findById(req.params.id);
+    if(!listing) return next(errorHandler(404, 'Listing not found'));
+    // if the listing exist, we want to check if the user is the owner of the listing
+    if(req.user.id !== listing.userRef) return next(errorHandler(401, 'You can delete only your own listing!'));
+    // if all ok
+    try {
+        await Listing.findByIdAndDelete(req.params.id); 
+        // without await, was seeing that entry is deleted but with page refresh it was showing again 
+        // - 2days to debug (: Clearing cache and doing npm install again
+        res.status(200).json('Listing had been deleted.');
     } catch (error) {
         next(error);
     }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -120,6 +120,23 @@ const Profile = () => {
       setShowListingsError(true);
     }
   };
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',        
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) => 
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -185,31 +202,29 @@ const Profile = () => {
       <button onClick={handleShowListings} className="text-green-700 w-full">Show Listings</button>
       <p>
         {showListingsError ? 'Error showing listings' : ''}
-      </p>
-      <p> 
-        {
-          userListings && userListings.length > 0 &&     
-          <div className="flex flex-col gap-4">
-            <h1 className="text-center text-2xl font-semibold mt-7">Your Listings</h1>
-            {
-              userListings.map((listing) => (
-                <div key={listing._id} className="flex border rounded-lg p-3 justify-between items-center gap-4">
-                  <Link to={`/listing/${listing._id}`}>
-                    <img src={listing.imageUrls[0]} alt="Listing cover" className="w-16 h-16 object-contain" />
-                  </Link>
-                  <Link to={`/listing/${listing._id}`} className="text-slate-700 font-semibold flex-1 hover:underline truncate">
-                    <p>{listing.name}</p>
-                  </Link>
-                  <div className="flex flex-col items-center">
-                    <button className="text-green-700">Edit</button>
-                    <button className="text-red-700">Delete</button>
-                  </div>
+      </p> 
+      {
+        userListings && userListings.length > 0 &&     
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center text-2xl font-semibold mt-7">Your Listings</h1>
+          {
+            userListings.map((listing) => (
+              <div key={listing._id} className="flex border rounded-lg p-3 justify-between items-center gap-4">
+                <Link to={`/listing/${listing._id}`}>
+                  <img src={listing.imageUrls[0]} alt="Listing cover" className="w-16 h-16 object-contain" />
+                </Link>
+                <Link to={`/listing/${listing._id}`} className="text-slate-700 font-semibold flex-1 hover:underline truncate">
+                  <p>{listing.name}</p>
+                </Link>
+                <div className="flex flex-col items-center">
+                  <button className="text-green-700">Edit</button>
+                  <button onClick={ () => handleDeleteListing(listing._id)} className="text-red-700">Delete</button>
                 </div>
-              ))
-            }
-          </div>               
-        }         
-      </p>
+              </div>
+            ))
+          }
+        </div>               
+      }   
     </div>
   )
 }
