a0d03eda4c14daf24a3a5c75920a58505ca82b90
Ranjit Madame <ranjitkumar.madame@mphasis.com>
2024-03-10 20:58:00 +0530

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold text-slate-800 mb-4'>About Thane Housing</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p className='mb-4 text-slate-700'>Felis eget nunc lobortis mattis aliquam faucibus purus in massa. 
+        Nisl suscipit adipiscing bibendum est. Hac habitasse platea dictumst quisque sagittis purus sit. 
+        Tellus in hac habitasse platea dictumst. Habitant morbi tristique senectus et netus et malesuada fames ac. 
+        At lectus urna duis convallis. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. </p>
+      <p className='mb-4 text-slate-700'>Dapibus ultrices in iaculis nunc sed augue. 
+        Sit amet commodo nulla facilisi nullam vehicula ipsum. 
+        Dui accumsan sit amet nulla. Vestibulum lorem sed risus ultricies tristique. 
+        Nibh praesent tristique magna sit amet purus gravida quis blandit. 
+        Et malesuada fames ac turpis egestas maecenas pharetra convallis posuere. 
+        Et netus et malesuada fames ac turpis egestas maecenas pharetra.</p>      
+    </div>
   )
 }
 
